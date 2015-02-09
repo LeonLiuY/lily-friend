9ea9a09d8203c1a5b637c1ecc6ec64454617d123
Liu Yang <liuyang1204@hotmail.com>
2015-02-10 01:40:57 +0800

    Preview score

--- app.rb
+++ app.rb
@@ -5,9 +5,28 @@ get '/' do
   send_file File.join(settings.public_folder, 'index.html')
 end
 
+NAME='music'
 
 post '/convert' do
   text = request.body.read
-  sleep 5
-  text.upcase
+  dir = Dir.mktmpdir
+  puts dir
+  File.open(File.join(dir, "#{NAME}.ly"), 'w') {|f|
+    f.write(text)
+  }
+  out = %x(cd #{dir} && lilypond -dbackend=svg #{NAME}.ly 2>&1)
+  puts out
+  exit_code = $?.exitstatus
+  if exit_code == 0
+    status 200
+    body dir
+  else
+    status 400
+    body out
+  end
+end
+
+get '/scores' do
+  path = params[:path]
+  send_file File.join(path, 'music.svg')
 end
