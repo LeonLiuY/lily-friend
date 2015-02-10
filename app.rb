e0785dec9b2c0cac1c7e2c0864b0d4fc128bb56b
Liu Yang <liu_yang@worksap.co.jp>
2015-02-10 17:44:18 +0800

    Improve layout

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-
+require 'json'
 
 get '/' do
   send_file File.join(settings.public_folder, 'index.html')
@@ -8,19 +8,23 @@ end
 NAME='music'
 
 post '/convert' do
-  text = request.body.read
+  content_type 'application/json'
+  text = request.body.read.strip
+  if text.empty?
+    halt 400, {out: 'Text should not be empty!'}.to_json
+  end
   dir = Dir.mktmpdir
-  File.open(File.join(dir, "#{NAME}.ly"), 'w') {|f|
+  File.open(File.join(dir, "#{NAME}.ly"), 'w') { |f|
     f.write(text)
   }
   out = %x(cd #{dir} && lilypond -dbackend=svg #{NAME}.ly 2>&1)
   exit_code = $?.exitstatus
   if exit_code == 0
     status 200
-    body dir
+    body ({dir: dir, out: out}.to_json)
   else
     status 400
-    body out
+    body ({out: out}.to_json)
   end
 end
 
